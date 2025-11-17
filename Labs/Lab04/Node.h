d0daecd0d119b86ea7fffe236bfc471c93f58abd
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2025-11-17 10:31:43 -0500

    Update CS246 repo - 2025-11-17 10:31:43

---
+++ Labs/Lab04/Node.h
@@ -0,0 +1,34 @@
+#ifndef NODE_H
+#define NODE_H
+
+namespace dsl
+{
+    template <class T>
+    class NodeS
+    {
+        public:
+            T data;
+            NodeS<T>* next;
+            NodeS(const T& value) // : data(value), next(nullptr) {}
+            {
+                data = value;
+                next = nullptr;
+            }
+    };
+
+    template <class T>
+    class NodeD
+    {
+        public:
+            T data;
+            NodeD<T>* next;
+            NodeD<T>* prev;
+            NodeD(const T& value) // : data(value), next(nullptr), prev(nullptr) {}
+            {
+                data = value;
+                next = nullptr;
+                prev = nullptr;
+            }
+    };
+}
+#endif
